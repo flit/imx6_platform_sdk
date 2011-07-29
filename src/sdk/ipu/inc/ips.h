ffb96dc9137d896bdc19bb3a05516f97aa3b20d8
Ray Sun <yanfei.sun@freescale.com>
2011-07-29 10:02:36 +0800

    update ips header
    Signed-off-by: Ray Sun <yanfei.sun@freescale.com>

--- src/sdk/ipu/inc/ips.h
+++ src/sdk/ipu/inc/ips.h
@@ -10,10 +10,10 @@
  * @brief IPU Software library macros, structures and functions 
  */
 
-#ifndef IPS_H
-#define IPS_H
+#ifndef _IPS_H_
+#define _IPS_H_
 
-/*--------------------------------- macros --------------------------------------*/
+/*ips flow status enumeration*/
 enum ips_flow_state_e {
     IPS_FLOW_STATE_NULL,
     IPS_FLOW_STATE_READY,
@@ -21,44 +21,64 @@ enum ips_flow_state_e {
     IPS_FLOW_STATE_PLAYING
 };
 
-/*-------------------------------- object structs -------------------------------*/
-struct ips_display_device_s {
-    uint32_t bus_width;         // up to 48bit
-    uint32_t disp_clk_pol;      // polarity of display clock: OFF, ON
-    uint32_t disp_clock_freq;   // up to 500MHz
-    uint32_t ext_hsync_en;      // synchronization with external HSYNC: OFF, ON
-    uint32_t ext_vsync_en;      // syncrhonization with external VYSNC: OFF, ON
-    uint32_t total_frame_height;    // shows display height(in lines). up to 0x4000
-    uint32_t active_window_left_offset; // 1, Frame of left corner (in pixels) position of active window
-    // 2, Display of inactive pixels is not
-    // up to 0x4000
-    uint32_t active_window_top_offset;  // 1, Frame of top corner (in pixels) position of active window
-    // 2, Display of inactive pixels is not
-    // up to 0x4000
-    uint32_t total_frame_width; // shows display width(in lines). up to 0x4000  
-    uint32_t h_active_end;      // Marks time period (in pixels) between last active pixel 
-    // in the refresh line and new HSYNC. up to 0x4000
-    uint32_t h_active_start;    // Marks time period (in pixels) between HSYNC pulse 
-    // and first active pixel in the refreshed line . up to 0x4000
-    uint32_t h_sync_width;      // Marks width of HSYNC pulse.
-    uint32_t if_type;           // select from list: BT1120, BT656, EAI861, JPEG, Sys8080, sys68
-    uint32_t *if_param_ptr;     // Pointer to interface specific parameter structure.
-    uint32_t v_active_end;      // Marks time period (in refreshed lines) between last active line and
-    // new VSYNC. up to 0x4000  
-    uint32_t v_active_start;    // Marks time period (in refreshed lines) between VSYNC pulse and first
-    // active line in the frame. up to 0x4000
-    uint32_t v_sync_width;      // Marks width of VSYNC pulse. up to 0x4000 
+enum ips_rotate_angle_e {
+    IPS_NO_ROTATE;
+    IPS_ROTATE_90;
+    IPS_ROTATE_180;
+    IPS_ROTATE_270;
 };
 
-typedef struct {
-    uint32_t data_pol;          // Polarity of data bus: OFF, ON
-    uint32_t enable_pol;        // Polarity of data enable signal (DRDY): OFF, ON
-    uint32_t h_sync_pol;        // Polarity of HSYNC pulse: OFF, ON
-    uint32_t v_to_h_sync;       // Offset of VSYNC to HSYNC (in pixels). up to 0x4000
-    uint32_t vsync_pol;         // Polarity of VSYNC pulse: OFF, ON
+/*!
+ * @brief IPS devices description.
+ */
 
-} _IPS_display_if_t;
+/*display device structure*/
+struct ips_dev_display_s {
+    uint32_t bus_width;
+    uint32_t disp_clk_pol;
+    uint32_t disp_clock_freq;
+    uint32_t ext_hsync_en;
+    uint32_t ext_vsync_en;
+    uint32_t total_frame_height;
+    uint32_t active_window_left_offset;
+    uint32_t active_window_top_offset;
+    uint32_t total_frame_width;
+    uint32_t h_active_end;
+    uint32_t h_active_start;
+    uint32_t h_sync_width;
+    uint32_t if_type;
+    uint32_t *if_param_ptr;     // point to the display interfaces definition
+    uint32_t v_active_end;
+    uint32_t v_active_start;
+    uint32_t v_sync_width;
+};
 
-/*--------------------------------- functions -------------------------------------*/
+struct ips_display_if_s {
+    uint32_t data_pol;          // Polarity of data bus
+    uint32_t enable_pol;        // Polarity of data enable signal (DRDY)
+    uint32_t h_sync_pol;        // Polarity of HSYNC pulse
+    uint32_t v_to_h_sync;       // Offset of VSYNC to HSYNC(in pixels)
+    uint32_t vsync_pol;         // Polarity of VSYNC pulse
+};
 
+/*memory device structure*/
+struct ips_dev_memory_s {
+    uint32_t active_frame_buf;
+    uint32_t number_frame_buf;
+    uint32_t stride_line_addr_0;
+    uint32_t stride_line_addr_1;
+    uint32_t stride_line_addr_2;
+    uint32_t stride_line_addr_3;
+};
+
+/*rotation device structure*/
+struct ips_dev_rotate_s {
+    uint32_t angle;
+}
+/*ipu device structure. ipu is super device combined of several devices */ struct ips_dev_memory_s {
+    struct ips_dev_memory_s mem;
+    struct ips_dev_rotate_s rotate;
+    struct ips_dev_resize_s resize;
+    struct ips_dev_display_s disp;
+};
 #endif

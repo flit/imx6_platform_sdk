3dc2e67d94ef008d31c368aa96ad0f1593c4864e
Sheng Nan <b38800@freescale.com>
2012-01-19 17:50:49 +0800

    1 enable camera parallel through IPU1 CSI0 2 add delay in camera config 3 add vdoa/drv in config/mx61_sabra_tablet

--- src/mx61/hardware.c
+++ src/mx61/hardware.c
@@ -711,8 +711,7 @@ int perfmon_clk_cfg(uint32_t base, uint32_t enable)
     }
 
     /* do nothing if shit = 0 <=> no base address match */
-    if(shift != 0)
-    {
+    if (shift != 0) {
         val = reg32_read(reg);
         if (enable) {
             val |= 0x03 << shift;
@@ -729,42 +728,55 @@ int perfmon_clk_cfg(uint32_t base, uint32_t enable)
 /*IOMUX configuration for CSI port0*/
 void mxc_csi_port0_iomux_config(void)
 {
+    uint32_t tmpVal;
+
     /* config CSI0_PIXCLK pad for ipu instance CSI0_PIXCLK port */
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_CSI0_PIXCLK, ALT0);
-    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_PIXCLK, 0x01000);
+    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_PIXCLK, 0x130B0);
     /* config CSI0_MCLK pad for ipu instance CSI0_HSYNC port */
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_CSI0_MCLK, ALT0);
-    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_MCLK, 0x01000);
+    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_MCLK, 0x130B0);
     /* config CSI0_DATA_EN pad for ipu instance CSI0_DATA_EN port */
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_CSI0_DATA_EN, ALT0);
-    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DATA_EN, 0x01000);
+    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DATA_EN, 0x130B0);
     /* config CSI0_VSYNC pad for ipu instance CSI0_VSYNC port */
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_CSI0_VSYNC, ALT0);
-    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_VSYNC, 0x01000);
+    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_VSYNC, 0x130B0);
     /* config CSI0_DAT12 pad for ipu instance CSI0_D[12] port */
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_CSI0_DAT12, ALT0);
-    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DAT12, 0x01000);
+    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DAT12, 0x130B0);
     /* config CSI0_DAT13 pad for ipu instance CSI0_D[13] port */
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_CSI0_DAT13, ALT0);
-    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DAT13, 0x01000);
+    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DAT13, 0x130B0);
     /* config CSI0_DAT14 pad for ipu instance CSI0_D[14] port */
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_CSI0_DAT14, ALT0);
-    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DAT14, 0x01000);
+    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DAT14, 0x130B0);
     /* config CSI0_DAT15 pad for ipu instance CSI0_D[15] port */
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_CSI0_DAT15, ALT0);
-    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DAT15, 0x01000);
+    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DAT15, 0x130B0);
     /* config CSI0_DAT16 pad for ipu instance CSI0_D[16] port */
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_CSI0_DAT16, ALT0);
-    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DAT16, 0x01000);
+    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DAT16, 0x130B0);
     /* config CSI0_DAT17 pad for ipu instance CSI0_D[17] port */
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_CSI0_DAT17, ALT0);
-    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DAT17, 0x01000);
+    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DAT17, 0x130B0);
     /* config CSI0_DAT18 pad for ipu instance CSI0_D[18] port */
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_CSI0_DAT18, ALT0);
-    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DAT18, 0x01000);
+    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DAT18, 0x130B0);
     /* config CSI0_DAT19 pad for ipu instance CSI0_D[19] port */
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_CSI0_DAT19, ALT0);
-    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DAT19, 0x01000);
+    reg32_write(IOMUXC_SW_PAD_CTL_PAD_CSI0_DAT19, 0x130B0);
+
+    /* set GPR1 to enable parallel interface
+     * bit 19: 0 - Enable mipi to IPU1 CSI0, virtual channel is fixed to 0
+     *         1 - Enable parallel interface to IPU CSI0
+     * bit 20: 0 - Enable mipi to IPU2 CSI1, virtual channel is fixed to 3
+     *         1 - Enable parallel interface to IPU2 CSI1
+     * IPU1 CSI1 directly connect to mipi CSI2, virtual channel is fixed to 1
+     * IPU2 CSI0 directly connect to mipi CSI2, virtual channel is fixed to 2
+     */
+    tmpVal = reg32_read(IOMUXC_GPR1);
+    reg32_write(IOMUXC_GPR1, tmpVal | (0x1 << 19));
 }
 
 /* ENET iomux config */

flit/imx6_platform_sdk

commit a8de2a70c2837384619a96662a9b593e92aecae7

ENGR176446 Split board/chip macros
- Added CHIP_MXx macros (BOARD_x macros were added in a previous commit).
- Removed old MX6x_y macros.
- Changed all uses of the old macros to the new style.

src/mx6dq/hardware.c
@@ -33,7 +33,7 @@ hw_module_t arm_core = {
     792000000,
 };
 
-#if  defined(MX6DQ_SABRE_LITE)
+#if  defined(BOARD_EVB)
 // UART2 is the serial debug/console port for sabre_lite board
 hw_module_t g_debug_uart = {
     "UART2 for debug",
@@ -43,7 +43,7 @@ hw_module_t g_debug_uart = {
     IMX_INT_UART2,
     &default_interrupt_routine,
 };
-#elif defined(MX6DQ_SMART_DEVICE)
+#elif defined(BOARD_SMART_DEVICE)
 // UART1 is the serial debug/console port for smart_device board
 hw_module_t g_debug_uart = {
     "UART1 for debug",
@@ -222,7 +222,7 @@ void show_ddr_config(void)
  */
 void board_init(void)
 {
-#if defined(MX6DQ_EVB)
+#if defined(BOARD_EVB)
     // Configure some board signals through I/O expanders
     max7310_i2c_req_array[0].ctl_addr = I2C3_BASE_ADDR; // the I2C controller base address
     max7310_i2c_req_array[0].dev_addr = MAX7310_I2C_ID0;    // the I2C DEVICE address
@@ -348,7 +348,7 @@ void ipu_iomux_config(void)
  */
 void lvds_power_on(void)
 {
-#ifdef MX6DQ_EVB
+#ifdef BOARD_EVB
     /*3.3V power supply through the load switch FDC6331L */
     max7310_set_gpio_output(0, 0, GPIO_HIGH_LEVEL);
     max7310_set_gpio_output(1, 1, GPIO_HIGH_LEVEL);
@@ -359,7 +359,7 @@ void lvds_power_on(void)
     gpio_write_data(GPIO_PORT1, 9, GPIO_HIGH_LEVEL);
 #endif
 
-#ifdef MX6DQ_SMART_DEVICE
+#ifdef BOARD_SMART_DEVICE
     // 3v3 on by default
     // AUX_5V_EN LVDS0 power
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_NANDF_RB0, ALT5);
@@ -372,7 +372,7 @@ void lvds_power_on(void)
     gpio_write_data(GPIO_PORT1, 21, GPIO_HIGH_LEVEL);
 #endif
 
-#ifdef MX6DQ_SABRE_AI
+#ifdef BOARD_SABRE_AI
     /*3.3V power supply through IOexpander */
     max7310_set_gpio_output(0, 0, GPIO_HIGH_LEVEL);
 
@@ -390,17 +390,17 @@ void lvds_power_on(void)
 void mipi_backlight_en(void)
 {
     //configure pin19 of the mipi dsi/csi connector
-#ifdef MX6DQ_EVB
+#ifdef BOARD_EVB
     //set GPIO1_9 to 0 so clear vbus on board
     gpio_dir_config(GPIO_PORT1, 9, GPIO_GDIR_OUTPUT);
     gpio_write_data(GPIO_PORT1, 9, GPIO_HIGH_LEVEL);
 #endif
 
-#ifdef MX6DQ_SABRE_AI
+#ifdef BOARD_SABRE_AI
     //default be populated by P3V3_DELAYED
 #endif
 
-#ifdef MX6DQ_SMART_DEVICE
+#ifdef BOARD_SMART_DEVICE
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_NANDF_D0, ALT5);
     reg32_write(IOMUXC_SW_PAD_CTL_PAD_NANDF_D0, 0x1B0B0);
     gpio_dir_config(GPIO_PORT2, 0, GPIO_GDIR_OUTPUT);
@@ -413,7 +413,7 @@ void mipi_backlight_en(void)
  */
 void mipi_display_reset(void)
 {
-#ifdef MX6DQ_EVB
+#ifdef BOARD_EVB
 /*pin29 of mipi connector for the LCD reset*/
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_EIM_WAIT, ALT5);
     reg32_write(IOMUXC_SW_PAD_CTL_PAD_EIM_WAIT, 0x1b0b0);
@@ -424,11 +424,11 @@ void mipi_display_reset(void)
     hal_delay_us(1000);
 #endif
 
-#ifdef MX6DQ_SABRE_AI
+#ifdef BOARD_SABRE_AI
 /*binded with the board reset button*/
 #endif
 
-#ifdef MX6DQ_SMART_DEVICE
+#ifdef BOARD_SMART_DEVICE
 /*pin29 of mipi connector for the LCD reset*/
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_NANDF_CS0, ALT5);
     reg32_write(IOMUXC_SW_PAD_CTL_PAD_NANDF_CS0, 0x1b0b0);
@@ -528,7 +528,7 @@ void sii9022_power_on(void)
 void can_iomux_config(uint32_t module_base_add)
 {
 
-#ifdef MX6DQ_SABRE_AI
+#ifdef BOARD_SABRE_AI
     /* CAN_EN active high output */
     max7310_set_gpio_output(1, 7, GPIO_HIGH_LEVEL); //expander b, io7
 
@@ -539,7 +539,7 @@ void can_iomux_config(uint32_t module_base_add)
     switch (module_base_add) {
     case CAN1_BASE_ADDR:
         can1_iomux_config();
-#ifdef MX6DQ_SABRE_AI
+#ifdef BOARD_SABRE_AI
         /* Select CAN, ENET_CAN1_STEER(PORT_EXP_B3) */
         max7310_set_gpio_output(1, 3, GPIO_HIGH_LEVEL); //expander b, io3 
         /* Select ALT5 mode of GPIO_4 for GPIO1_4 - CAN1_NERR_B */
@@ -550,7 +550,7 @@ void can_iomux_config(uint32_t module_base_add)
         break;
     case CAN2_BASE_ADDR:
         can2_iomux_config();
-#ifdef MX6DQ_SABRE_AI
+#ifdef BOARD_SABRE_AI
         /* Select ALT5 mode of SD4_DAT3 for GPIO2_11 - CAN2_NERR_B */
         /* active low input */
         writel(ALT5, IOMUXC_SW_MUX_CTL_PAD_SD4_DAT3);
@@ -655,7 +655,7 @@ void SGTL5000PowerUp_and_clockinit(void)
  */
 int esai_codec_power_on(void)
 {
-    //No need to do anything for MX6DQ_SABRE_AI
+    //No need to do anything for BOARD_SABRE_AI
     return 0;
 }
 

src/mx6sdl/hardware.c
@@ -31,7 +31,7 @@ hw_module_t arm_core = {
     792000000,
 };
 
-#if  defined(MX6SDL_SABRE_LITE)
+#if  defined(BOARD_SABRE_LITE)
 // UART2 is the serial debug/console port for sabre_lite board
 hw_module_t g_debug_uart = {
     "UART2 for debug",
@@ -41,7 +41,7 @@ hw_module_t g_debug_uart = {
     IMX_INT_UART2,
     &default_interrupt_routine,
 };
-#elif defined(MX6SDL_SMART_DEVICE)
+#elif defined(BOARD_SMART_DEVICE)
 // UART1 is the serial debug/console port for smart_device board
 hw_module_t g_debug_uart = {
     "UART1 for debug",
@@ -220,7 +220,7 @@ void show_ddr_config(void)
  */
 void board_init(void)
 {
-#if defined(MX6SDL_EVB) || defined(MX6SDL_ARD)
+#if defined(BOARD_EVB) || defined(BOARD_SABRE_AI)
     // Configure some board signals through I/O expanders
     max7310_i2c_req_array[0].ctl_addr = I2C3_BASE_ADDR; // the I2C controller base address
     max7310_i2c_req_array[0].dev_addr = MAX7310_I2C_ID0;    // the I2C DEVICE address
@@ -228,7 +228,7 @@ void board_init(void)
     max7310_i2c_req_array[1].ctl_addr = I2C3_BASE_ADDR; // the I2C controller base address
     max7310_i2c_req_array[1].dev_addr = MAX7310_I2C_ID1;    // the I2C DEVICE address
     max7310_init(1, MAX7310_ID1_DEF_DIR, MAX7310_ID1_DEF_VAL);
-#if defined(MX6SDL_ARD)
+#if defined(BOARD_SABRE_AI)
     max7310_i2c_req_array[2].ctl_addr = I2C3_BASE_ADDR; // the I2C controller base address
     max7310_i2c_req_array[2].dev_addr = MAX7310_I2C_ID2;    // the I2C DEVICE address
     max7310_init(2, MAX7310_ID2_DEF_DIR, MAX7310_ID2_DEF_VAL);
@@ -349,7 +349,7 @@ void ipu_iomux_config(void)
  */
 void lvds_power_on(void)
 {
-#if defined(MX6SDL_EVB)
+#if defined(BOARD_EVB)
     /*3.3V power supply through the load switch FDC6331L */
     max7310_set_gpio_output(0, 0, GPIO_HIGH_LEVEL);
     max7310_set_gpio_output(1, 1, GPIO_HIGH_LEVEL);
@@ -360,7 +360,7 @@ void lvds_power_on(void)
     gpio_write_data(GPIO_PORT1, 9, GPIO_HIGH_LEVEL);
 #endif
 
-#ifdef MX6SDL_SMART_DEVICE
+#ifdef BOARD_SMART_DEVICE
     // 3v3 on by default
     // AUX_5V_EN LVDS0 power
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_NANDF_RB0, ALT5);
@@ -373,7 +373,7 @@ void lvds_power_on(void)
     gpio_write_data(GPIO_PORT1, 21, GPIO_HIGH_LEVEL);
 #endif
 
-#ifdef MX6SDL_ARD
+#ifdef BOARD_SABRE_AI
     /*3.3V power supply through IOexpander */
     max7310_set_gpio_output(0, 0, GPIO_HIGH_LEVEL);
 
@@ -391,17 +391,17 @@ void lvds_power_on(void)
 void mipi_backlight_en(void)
 {
     //configure pin19 of the mipi dsi/csi connector
-#ifdef MX6SDL_EVB
+#ifdef BOARD_EVB
     //set GPIO1_9 to 0 so clear vbus on board
     gpio_dir_config(GPIO_PORT1, 9, GPIO_GDIR_OUTPUT);
     gpio_write_data(GPIO_PORT1, 9, GPIO_HIGH_LEVEL);
 #endif
 
-#ifdef MX6SDL_ARD
+#ifdef BOARD_SABRE_AI
     //default be populated by P3V3_DELAYED
 #endif
 
-#ifdef MX6SDL_SMART_DEVICE
+#ifdef BOARD_SMART_DEVICE
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_NANDF_D0, ALT5);
     reg32_write(IOMUXC_SW_PAD_CTL_PAD_NANDF_D0, 0x1B0B0);
     gpio_dir_config(GPIO_PORT2, 0, GPIO_GDIR_OUTPUT);
@@ -414,7 +414,7 @@ void mipi_backlight_en(void)
  */
 void mipi_display_reset(void)
 {
-#ifdef MX6SDL_EVB
+#ifdef BOARD_EVB
 /*pin29 of mipi connector for the LCD reset*/
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_EIM_WAIT, ALT5);
     reg32_write(IOMUXC_SW_PAD_CTL_PAD_EIM_WAIT, 0x1b0b0);
@@ -425,11 +425,11 @@ void mipi_display_reset(void)
     hal_delay_us(1000);
 #endif
 
-#ifdef MX6SDL_ARD
+#ifdef BOARD_SABRE_AI
 /*binded with the board reset button*/
 #endif
 
-#ifdef MX6SDL_SMART_DEVICE
+#ifdef BOARD_SMART_DEVICE
 /*pin29 of mipi connector for the LCD reset*/
     reg32_write(IOMUXC_SW_MUX_CTL_PAD_NANDF_CS0, ALT5);
     reg32_write(IOMUXC_SW_PAD_CTL_PAD_NANDF_CS0, 0x1b0b0);
@@ -528,7 +528,7 @@ void sii9022_power_on(void)
 void can_iomux_config(uint32_t module_base_add)
 {
 
-#ifdef MX6SDL_ARD
+#ifdef BOARD_SABRE_AI
     /* CAN_EN active high output */
     max7310_set_gpio_output(1, 7, GPIO_HIGH_LEVEL); //expander b, io7
 
@@ -539,7 +539,7 @@ void can_iomux_config(uint32_t module_base_add)
     switch (module_base_add) {
     case CAN1_BASE_ADDR:
         can1_iomux_config();
-#ifdef MX6SDL_ARD
+#ifdef BOARD_SABRE_AI
         /* Select CAN, ENET_CAN1_STEER(PORT_EXP_B3) */
         max7310_set_gpio_output(1, 3, GPIO_HIGH_LEVEL); //expander b, io3 
         /* Select ALT5 mode of GPIO_4 for GPIO1_4 - CAN1_NERR_B */
@@ -550,7 +550,7 @@ void can_iomux_config(uint32_t module_base_add)
         break;
     case CAN2_BASE_ADDR:
         can2_iomux_config();
-#ifdef MX6SDL_ARD
+#ifdef BOARD_SABRE_AI
         /* Select ALT5 mode of SD4_DAT3 for GPIO2_11 - CAN2_NERR_B */
         /* active low input */
         writel(ALT5, IOMUXC_SW_MUX_CTL_PAD_SD4_DAT3);

make.def
@@ -30,7 +30,7 @@ MAKE = make
 # MX53
 #
 ifeq "$(TARGET)" "mx53"
-C_DEFINES +=-DMX53
+C_DEFINES +=-DCHIP_MX53
 CPU	=cortex-a8
 ifeq "$(BOARD)" "evk"
 C_DEFINES +=-DMX53_EVK
@@ -53,42 +53,21 @@ endif
 # MX6DQ
 #
 ifeq "$(TARGET)" "mx6dq"
-C_DEFINES += -DMX6DQ -DCHIP_MX6DQ
-CPU	=cortex-a9
-ifeq "$(BOARD)" "evb"
-C_DEFINES +=-DMX6DQ_EVB
-endif
-ifeq "$(BOARD)" "sabre_ai"
-C_DEFINES +=-DMX6DQ_SABRE_AI
-endif
-ifeq "$(BOARD)" "sabre_lite"
-C_DEFINES +=-DMX6DQ_SABRE_LITE
-endif
-ifeq "$(BOARD)" "smart_device"
-C_DEFINES +=-DMX6DQ_SMART_DEVICE
-endif
+C_DEFINES += -DCHIP_MX6DQ
+CPU	= cortex-a9
 endif
 
 #
 # MX6SDL
 #
 ifeq "$(TARGET)" "mx6sdl"
-C_DEFINES +=-DMX6SDL -DCHIP_MX6SDL
-CPU	=cortex-a9
-ifeq "$(BOARD)" "evb"
-C_DEFINES +=-DMX6SDL_EVB
-endif
-ifeq "$(BOARD)" "sabre_ai"
-C_DEFINES +=-DMX6SDL_SABRE_AI
-endif
-ifeq "$(BOARD)" "sabre_lite"
-C_DEFINES +=-DMX6SDL_SABRE_LITE
-endif
-ifeq "$(BOARD)" "smart_device"
-C_DEFINES +=-DMX6SDL_SMART_DEVICE
-endif
+C_DEFINES += -DCHIP_MX6SDL
+CPU	= cortex-a9
 endif
 
+#
+# Board
+#
 ifeq "$(BOARD)" "evb"
 C_DEFINES += -DBOARD_EVB
 endif
@@ -102,6 +81,9 @@ ifeq "$(BOARD)" "smart_device"
 C_DEFINES += -DBOARD_SMART_DEVICE
 endif
 
+#
+# Board revision
+#
 ifeq "$(BOARD_REVISION)" "b"
 C_DEFINES +=-DBOARD_VERSION2
 endif

src/common/hdmi_transmitter/sii902x/drv/siHdmiTx_902x_TPI.c
@@ -221,7 +221,7 @@ void TxHW_Reset(void)
 {
     TPI_TRACE_PRINT((">>TxHW_Reset()\n"));
 
-#if defined(MX6DQ_SMART_DEVICE)||defined(MX6SDL_SMART_DEVICE)||defined(MX6DQ_EVB)||defined(MX6SDL_EVB)
+#if defined(BOARD_SMART_DEVICE)||defined(BOARD_EVB)
     /*sil9024 hardware reset */
     writel(0x5, IOMUXC_SW_MUX_CTL_PAD_EIM_WAIT);
     writel(0xB060, IOMUXC_SW_PAD_CTL_PAD_EIM_WAIT);

src/include/mx6dq/buffers.h
@@ -35,12 +35,12 @@
 /* put the TWO video instance on different CS to
    improve the performance.
  */
-#if defined(MX6DQ_EVB)
+#if defined(BOARD_EVB)
 #define FAT_DRIVER_BUFFER       (0x60000000)
 #define VPU_WORK_BUFFERS        (0x60100000)
 #define VIDEO_BUFFERS_START     (0x70000000)
 #define VIDEO_BUFFERS_END       (0x90000000)
-#elif defined(MX6DQ_SMART_DEVICE) || defined(MX6DQ_SABRE_LITE)
+#elif defined(BOARD_SMART_DEVICE) || defined(BOARD_SABRE_LITE)
 #define FAT_DRIVER_BUFFER       (0x30000000)
 #define VPU_WORK_BUFFERS        (0x30100000)
 #define VIDEO_BUFFERS_START     (0x20000000)

src/include/mx6dq/hardware.h
@@ -56,14 +56,14 @@
 #define WEIM_REGISTERS_BASE_ADDR WEIM_BASE_ADDR
 #define WEIM_CS_BASE_ADDR   0x08000000
 
-#if defined(MX6DQ_EVB) || defined(MX6DQ_SMART_DEVICE) || defined(MX6DQ_SABRE_LITE)
+#if defined(BOARD_EVB) || defined(BOARD_SMART_DEVICE) || defined(BOARD_SABRE_LITE)
 #define MMDC0_BASE_ADDR			0x10000000
 #define MMDC0_END_ADDR			0x4FFFFFFF
 /*For SMD and QSB, there is no DDR_CS1, just define them to avoid build error, but the memory region can not be accessed */
 #define MMDC1_BASE_ADDR			0x50000000
 #define MMDC1_END_ADDR			0x8FFFFFFF
 #endif
-#if defined(MX6DQ_SABRE_AI)
+#if defined(BOARD_SABRE_AI)
 #define MMDC0_BASE_ADDR			0x10000000
 #define MMDC0_END_ADDR			0x8FFFFFFF  //Maybe should be modified according the ddr init file.
 /*Actually, ARD has no DDR_CS1, just define them to avoid build error, but the memory region can not be accessed */
@@ -105,16 +105,16 @@
 #define ADV7180_I2C_ID      (0x42 >> 1)
 
 /* 
- * MX6DQ_SMART_DEVICE/MX6DQ_SABRE_LITE for compile error
+ * BOARD_SMART_DEVICE/BOARD_SABRE_LITE for compile error
  */
-#if defined(MX6DQ_SMART_DEVICE) || defined(MX6DQ_SABRE_LITE)
+#if defined(BOARD_SMART_DEVICE) || defined(BOARD_SABRE_LITE)
 #define MAX7310_NBR 0
 #endif
 /*******************************************************
  *      I/O expander MAX7310 I2C settings
  *******************************************************/
 /* For the ARD board which has 3 MAX7310 */
-#ifdef MX6DQ_SABRE_AI
+#ifdef BOARD_SABRE_AI
 #define MAX7310_NBR 3
 /* I/O expander A */
 #define MAX7310_I2C_BASE_ID0  I2C3_BASE_ADDR
@@ -135,7 +135,7 @@
 #define MAX7310_ID2_DEF_VAL      0x57   // init value for the output
 #endif
 
-#ifdef MX6DQ_EVB
+#ifdef BOARD_EVB
 /* For the EVB board which has 2 MAX7310 */
 #define MAX7310_NBR 2
 /* Number 1 controls: BACKLIGHT_ON, PORT3_P114, CPU_PER_RST_B, PORT3_P110,
@@ -296,13 +296,13 @@ extern void csi_port0_iomux_config(void);
 #error Need to define a board revision
 #endif
 
-#if defined(MX6DQ_SABRE_AI)
+#if defined(BOARD_SABRE_AI)
 #define BOARD_TYPE_ID           BOARD_ID_MX6DQ_SABRE_AI
-#elif defined(MX6DQ_SMART_DEVICE)
+#elif defined(BOARD_SMART_DEVICE)
 #define BOARD_TYPE_ID           BOARD_ID_MX6DQ_SMART_DEVICE
-#elif defined(MX6DQ_EVB)
+#elif defined(BOARD_EVB)
 #define BOARD_TYPE_ID           BOARD_ID_MX6DQ_EVB
-#elif defined(MX6DQ_SABRE_LITE)
+#elif defined(BOARD_SABRE_LITE)
 #define BOARD_TYPE_ID           BOARD_ID_MX6DQ_SABRE_LITE
 #else
 #error Need to define a board type

src/include/mx6dq/plat_startup.h
@@ -80,7 +80,7 @@ dcd_write_data_cmd: .byte 0xCC      /* Tag=0xCC */
  */
 
 dcd_data_start:
-#if defined(MX6DQ_SABRE_AI) || defined(MX6DQ_EVB)
+#if defined(BOARD_SABRE_AI) || defined(BOARD_EVB)
     /*Use default DDR frequency: 528MHz*/
     DCD_DATA(IOMUXC_SW_PAD_CTL_PAD_DRAM_SDQS0, 0x00000030)
     DCD_DATA(IOMUXC_SW_PAD_CTL_PAD_DRAM_SDQS1, 0x00000030)
@@ -226,9 +226,9 @@ dcd_data_start:
     DCD_DATA(HW_MMDC_MPMUR0_ADDR(1), 0x00000800)
     DCD_DATA(HW_MMDC_MDSCR_ADDR(0), 0x00000000)
 #endif
-#endif /* ((defined MX6DQ_SABRE_AI) || (defined MX6DQ_EVB)) */
+#endif /* ((defined BOARD_SABRE_AI) || (defined BOARD_EVB)) */
 
-#if defined(MX6DQ_SMART_DEVICE)
+#if defined(BOARD_SMART_DEVICE)
     /*Use default DDR frequency: 528MHz*/
     DCD_DATA(IOMUXC_BASE_ADDR + 0x5a8, 0x00000028)
     DCD_DATA(IOMUXC_BASE_ADDR + 0x5b0, 0x00000028)
@@ -317,9 +317,9 @@ dcd_data_start:
     DCD_DATA(IOMUXC_BASE_ADDR + 0x010, 0xF00000FF)
     DCD_DATA(IOMUXC_BASE_ADDR + 0x018, 0x00070007)
     DCD_DATA(IOMUXC_BASE_ADDR + 0x01C, 0x00070007)
-#endif /* (defined MX6DQ_SMART_DEVICE) */
+#endif /* (defined BOARD_SMART_DEVICE) */
 
-#if defined(MX6DQ_SABRE_LITE)
+#if defined(BOARD_SABRE_LITE)
     DCD_DATA(IOMUXC_BASE_ADDR + 0x5a8, 0x00000030)
     DCD_DATA(IOMUXC_BASE_ADDR + 0x5b0, 0x00000030)
     DCD_DATA(IOMUXC_BASE_ADDR + 0x524, 0x00000030)
@@ -416,7 +416,7 @@ dcd_data_start:
     /* set IPU AXI-id0 Qos=0xf(bypass) AXI-id1 Qos=0x7 */
     DCD_DATA(IOMUXC_BASE_ADDR + 0x018, 0x007f007f)
     DCD_DATA(IOMUXC_BASE_ADDR + 0x01c, 0x007f007f)
-#endif /* (defined MX6DQ_SABRE_LITE) */
+#endif /* (defined BOARD_SABRE_LITE) */
 dcd_data_end:
 
 .endm   //plat_dcd_startup

src/include/mx6sdl/buffers.h
@@ -35,12 +35,12 @@
 /* put the TWO video instance on different CS to
    improve the performance.
  */
-#if defined(MX6SDL_EVB)
+#if defined(BOARD_EVB)
 #define FAT_DRIVER_BUFFER       (0x60000000)
 #define VPU_WORK_BUFFERS        (0x60100000)
 #define VIDEO_BUFFERS_START     (0x70000000)
 #define VIDEO_BUFFERS_END       (0x90000000)
-#elif defined(MX6SDL_SMART_DEVICE) || defined(MX6SDL_SABRE_LITE)
+#elif defined(BOARD_SMART_DEVICE) || defined(BOARD_SABRE_LITE)
 #define FAT_DRIVER_BUFFER       (0x30000000)
 #define VPU_WORK_BUFFERS        (0x30100000)
 #define VIDEO_BUFFERS_START     (0x20000000)

commit a3bfa9aea8d01b43c6b0254460f782aa9e72b9cc

Fixed platform.c to make the BOARD_EVB use the correct UART for the console.   (problem introduced by Commit a8de2a70c2837384619a96662a9b593e92aecae7)
Signed-off-by: Florent Auger <florent.auger@freescale.com>

src/mx6dq/hardware.c
@@ -33,7 +33,7 @@ hw_module_t arm_core = {
     792000000,
 };
 
-#if  defined(BOARD_EVB)
+#if  defined(BOARD_SABRE_LITE)
 // UART2 is the serial debug/console port for sabre_lite board
 hw_module_t g_debug_uart = {
     "UART2 for debug",
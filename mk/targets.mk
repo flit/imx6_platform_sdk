ff57f5f0d443c8d90233e9405d969f0bf40c8084
Chris Reed <creed@freescale.com>
2012-07-30 18:40:22 -0500

    Added support for source files in subdirectories.
    - The make system now supports source files in child directories.
    - Updated makefiles to make use of this feature.

--- mk/targets.mk
+++ mk/targets.mk
@@ -5,21 +5,40 @@ else
 OBJS_ROOT = $(LIB_OBJS_ROOT)
 endif
 
-# Separate assembler sources into .s and .S lists.
-ASM_s_SOURCES = $(filter %.s,$(ASM_SOURCES))
-ASM_S_SOURCES = $(filter %.S,$(ASM_SOURCES))
+# Kludge to create a variable equal to a single space.
+empty :=
+space := $(empty) $(empty)
+
+# Strip sources.
+SOURCES := $(strip $(SOURCES))
+
+# Convert sources list to absolute paths and relative paths.
+SOURCES_ABS := $(foreach s,$(SOURCES),$(abspath $(s)))
+SOURCES_REL := $(subst $(SDK_ROOT)/,,$(SOURCES_ABS))
+
+# Get a list of directories containing the source files.
+SOURCE_DIRS_ABS := $(sort $(foreach f,$(SOURCES_ABS),$(dir $(f))))
+SOURCE_DIRS_REL := $(subst $(SDK_ROOT)/,,$(SOURCE_DIRS_ABS))
+
+OBJECTS_DIRS := $(addprefix $(OBJS_ROOT)/,$(SOURCE_DIRS_REL))
+
+# Filter source files list into separate source types.
+C_SOURCES = $(filter %.c,$(SOURCES_REL))
+CXX_SOURCES = $(filter %.cpp,$(SOURCES_REL))
+ASM_s_SOURCES = $(filter %.s,$(SOURCES_REL))
+ASM_S_SOURCES = $(filter %.S,$(SOURCES_REL))
 
 # Convert sources to objects.
-OBJECTS_C := $(addprefix $(OBJS_ROOT)/,$(notdir $(C_SOURCES:.c=.o)))
-OBJECTS_CXX := $(addprefix $(OBJS_ROOT)/,$(notdir $(CXX_SOURCES:.c=.o)))
-OBJECTS_ASM := $(addprefix $(OBJS_ROOT)/,$(notdir $(ASM_s_SOURCES:.s=.o)))
-OBJECTS_ASM_S := $(addprefix $(OBJS_ROOT)/,$(notdir $(ASM_S_SOURCES:.S=.o)))
+OBJECTS_C := $(addprefix $(OBJS_ROOT)/,$(C_SOURCES:.c=.o))
+OBJECTS_CXX := $(addprefix $(OBJS_ROOT)/,$(CXX_SOURCES:.c=.o))
+OBJECTS_ASM := $(addprefix $(OBJS_ROOT)/,$(ASM_s_SOURCES:.s=.o))
+OBJECTS_ASM_S := $(addprefix $(OBJS_ROOT)/,$(ASM_S_SOURCES:.S=.o))
 
 # Complete list of all object files.
 OBJECTS_ALL := $(OBJECTS_LDS) $(OBJECTS_C) $(OBJECTS_CXX) $(OBJECTS_ASM) $(OBJECTS_ASM_S)
 
-ifneq "$(TARGET_LIB_NAME)" ""
 # Build the target lib path from the lib name.
+ifneq "$(TARGET_LIB_NAME)" ""
 TARGET_LIB ?= $(LIBS_ROOT)/$(TARGET_LIB_NAME)
 endif
 
@@ -30,11 +49,48 @@ endif
 # if subdirs modified the library file after local files were compiled but before they were added
 # to the library.
 .PHONY: all
-all: $(SUBDIRS) $(OBJS_ROOT) $(OBJECTS_ALL) $(LIBS) $(APP) $(TARGET_LIB)
+all : echovars $(SUBDIRS) $(OBJS_ROOT) $(OBJECTS_DIRS) $(OBJECTS_ALL) $(LIBS) $(APP) $(TARGET_LIB)
+
+.PHONY: echovars
+echovars:
+# 	@echo SOURCES = $(SOURCES)
+# 	@echo SOURCES_ABS = $(SOURCES_ABS)
+# 	@echo SOURCES_REL = $(SOURCES_REL)
+# 	@echo SOURCE_DIRS_PATH = $(SOURCE_DIRS_PATH)
+# 	@echo OBJECTS_DIRS = $(OBJECTS_DIRS)
+# 	@echo C_SOURCES = $(C_SOURCES)
+# 	@echo OBJECTS_C = $(OBJECTS_C)
 
 # Recipe to create the output directory.
-$(OBJS_ROOT):
-	mkdir -p $(OBJS_ROOT)
+$(OBJS_ROOT) $(OBJECTS_DIRS) :
+	@mkdir -p $@
+
+# Pattern rules for compilation.
+
+$(OBJS_ROOT)/%.o: $(SDK_ROOT)/%.c
+#%.o: %.c
+# Generate make dependencies and compile. The -MP option creates empty targets
+# for each header file so that a rebuild will be forced if the file goes missing, but
+# no error will occur.
+	@echo Compiling $(subst $(SDK_ROOT)/,,$<)
+	@$(CC) $(CFLAGS) $(SYSTEM_INC) $(C_INCLUDES) $(C_DEFINES) -MMD -MF $(basename $@).d -MP -o $@ -c $<
+
+$(OBJS_ROOT)/%.o: $(SDK_ROOT)/%.cpp
+# Generate make dependencies and compile. The -MP option creates empty targets
+# for each header file so that a rebuild will be forced if the file goes missing, but
+# no error will occur.
+	@echo Compiling $(subst $(SDK_ROOT)/,,$<)
+	@$(CXX) $(CXXFLAGS) $(SYSTEM_INC) $(C_INCLUDES) $(C_DEFINES) -MMD -MF $(basename $@).d -MP -o $@ -c $<
+
+# For .S assembly files, first run through the C preprocessor then assemble.
+$(OBJS_ROOT)/%.o: $(SDK_ROOT)/%.S
+	@echo Compiling $(subst $(SDK_ROOT)/,,$<)
+	@$(CPP) -D__LANGUAGE_ASM__ $(C_INCLUDES) $(C_DEFINES) -o $(basename $@).s $<
+	@$(AS) $(A_FLAGS) $(C_INCLUDES) $(A_DEFINES) -MD $(OBJS_ROOT)/$*.d -o $@ $(basename $@).s
+
+$(OBJS_ROOT)/%.o: $(SDK_ROOT)/%.s
+	@echo Compiling $(subst $(SDK_ROOT)/,,$<)
+	@$(AS) $(A_FLAGS) $(C_INCLUDES) $(A_DEFINES) -MD $(basename $@).d -o $@ $<
 
 
 # $(OBJECTS): %.o: %.c
@@ -49,31 +105,31 @@ $(OBJS_ROOT):
 # 	$(CC) -s $(OBJECTS_LDS).S $(CFLAGS) -E -P -D__ASM__ $(INC) -o $(OBJECTS_LDS)
 
 
+# $(OBJS_ROOT)/%.o: %.c
+# # Generate make dependencies and compile. The -MP option creates empty targets
+# # for each header file so that a rebuild will be forced if the file goes missing, but
+# # no error will occur.
+# #	@echo Compiling $(addprefix $(MODULE_REL_DIR)$(MODULE)/,$<)
+# 	@echo Compiling $<
+# 	@$(CC) $(CFLAGS) $(SYSTEM_INC) $(C_INCLUDES) $(C_DEFINES) -MMD -MF $(OBJS_ROOT)/$(basename $@).d -MP -o $@ -c $<
+# 
+# $(OBJS_ROOT)/%.o: %.cpp
+# # Generate make dependencies and compile. The -MP option creates empty targets
+# # for each header file so that a rebuild will be forced if the file goes missing, but
+# # no error will occur.
+# 	@echo Compiling $<
+# 	@$(CXX) $(CXXFLAGS) $(SYSTEM_INC) $(C_INCLUDES) $(C_DEFINES) -MMD -MF $(OBJS_ROOT)/$*.d -MP -o $@ -c $<
+# 
+# # For .S assembly files, first run through the C preprocessor then assemble.
+# $(OBJS_ROOT)/%.o: %.S
+# 	@echo Assembling $<
+# 	@$(CPP) -D__LANGUAGE_ASM__ $(C_INCLUDES) $(C_DEFINES) -o $(basename $@).s $<
+# 	@$(AS) $(A_FLAGS) $(C_INCLUDES) $(A_DEFINES) -MD $(OBJS_ROOT)/$*.d -o $@ $(basename $@).s
+# 
+# $(OBJS_ROOT)/%.o: %.s
+# 	@echo Assembling $<
+# 	@$(AS) $(A_FLAGS) $(C_INCLUDES) $(A_DEFINES) -MD $(OBJS_ROOT)/$*.d -o $@ $<
 
-$(OBJS_ROOT)/%.o: %.c
-# Generate make dependencies and compile. The -MP option creates empty targets
-# for each header file so that a rebuild will be forced if the file goes missing, but
-# no error will occur.
-#	@echo Compiling $(addprefix $(MODULE_REL_DIR)$(MODULE)/,$<)
-	@echo Compiling $<
-	@$(CC) $(CFLAGS) $(SYSTEM_INC) $(C_INCLUDES) $(C_DEFINES) -MMD -MF $(OBJS_ROOT)/$*.d -MP -o $@ -c $<
-
-$(OBJS_ROOT)/%.o: %.cpp
-# Generate make dependencies and compile. The -MP option creates empty targets
-# for each header file so that a rebuild will be forced if the file goes missing, but
-# no error will occur.
-	@echo Compiling $<
-	@$(CXX) $(CXXFLAGS) $(SYSTEM_INC) $(C_INCLUDES) $(C_DEFINES) -MMD -MF $(OBJS_ROOT)/$*.d -MP -o $@ -c $<
-
-# For .S assembly files, first run through the C preprocessor then assemble.
-$(OBJS_ROOT)/%.o: %.S
-	@echo Assembling $<
-	@$(CPP) -D__LANGUAGE_ASM__ $(C_INCLUDES) $(C_DEFINES) -o $(basename $@).s $<
-	@$(AS) $(A_FLAGS) $(C_INCLUDES) $(A_DEFINES) -MD $(OBJS_ROOT)/$*.d -o $@ $(basename $@).s
-
-$(OBJS_ROOT)/%.o: %.s
-	@echo Assembling $<
-	@$(AS) $(A_FLAGS) $(C_INCLUDES) $(A_DEFINES) -MD $(OBJS_ROOT)/$*.d -o $@ $<
 
 # Add objects to the target library.
 $(TARGET_LIB): $(OBJECTS_ALL)
@@ -83,7 +139,7 @@ $(TARGET_LIB): $(OBJECTS_ALL)
 # Recursively execute make in the subdirectories.
 .PHONY: $(SUBDIRS)
 $(SUBDIRS):
-	$(MAKE) -C $@
+	$(MAKE) -r -C $@
 
 # Link the application.
 $(APP): $(OBJECTS) $(OBJECTS_ASM) $(LIBRARIES) $(APP_LIBS)

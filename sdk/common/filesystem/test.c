72b189a6f2bcd8aa8a4f72535bf50f04aff380c7
Jerry Fan <b32609@shlx4.freescale.com>
2012-09-05 09:24:34 +0800

    Added file writing speed test, not tested yet, since the file system does not support writting currently.
    Signed-off-by: Jerry Fan <b32609@shlx4.freescale.com>

--- sdk/common/filesystem/test.c
+++ sdk/common/filesystem/test.c
@@ -18,12 +18,12 @@
 
 #include "hardware.h"
 #include "timer/epit.h"
-#include "usdhc/inc/usdhc_ifc.h"
+#include "usdhc/usdhc_ifc.h"
 
 uint8_t readfile[] = "indir/fsproj.h";
-uint8_t writefile[] = "outdir/fsproj.h";
+uint8_t writefile[] = "fs_test.dat";
 
-#define TEST_FILE_SIZE 2048
+#define TEST_FILE_SIZE (1024*1024*16)
 #define DeviceNum 0
 
 extern void print_media_fat_info(uint32_t);
@@ -37,6 +37,77 @@ hw_module_t count_timer = {
     NULL,
     NULL
 };
+
+int fat_write_speed_test(void)
+{
+	uint8_t *WriteBuffer = NULL;
+    uint32_t ClusterSize = 0, BytesWritten = 0;
+    uint32_t TimeCount = 0;
+	int32_t i = 0, count = 0;
+	int32_t fout;
+	
+    printf("FSInit				");
+    if (FSInit(NULL, bufy, maxdevices, maxhandles, maxcaches) != SUCCESS) {
+        printf("FAIL\n");
+		return ERROR_GENERIC;
+    } else {
+        printf("PASS\n");
+    }
+
+    /*init the drive */
+    FSDriveInit(DeviceNum);
+    SetCWDHandle(DeviceNum);
+
+
+    print_media_fat_info(DeviceNum);
+
+    if ((fout = Fopen(writefile, (uint8_t *) "wb+")) < 0) {
+        printf("Can't open the file: %s !\n", writefile);
+        return ERROR_GENERIC;
+    }
+
+    ClusterSize = MediaTable[DeviceNum].SectorsPerCluster * MediaTable[DeviceNum].BytesPerSector;
+	WriteBuffer = (uint8_t *)malloc(ClusterSize);
+    if (WriteBuffer == NULL) {
+        printf("Allocating buffer failed!\n");
+        return ERROR_GENERIC;
+    }
+	for(i = 0; i < ClusterSize; i++){
+		*WriteBuffer = (uint8_t)i;
+	}
+
+    count_timer.freq = get_main_clock(IPG_CLK);
+    epit_init(&count_timer, CLKSRC_IPG_CLK, count_timer.freq / 1000000,
+              SET_AND_FORGET, 10000, WAIT_MODE_EN | STOP_MODE_EN);
+    epit_setup_interrupt(&count_timer, FALSE);
+    epit_counter_enable(&count_timer, 0xFFFFFFFF, 0);   //polling mode
+
+    SDHC_ADMA_mode = 1;
+    SDHC_INTR_mode = 0;
+
+	while(BytesWritten < TEST_FILE_SIZE){
+		if((count = Fwrite(fout, WriteBuffer, ClusterSize)) != ClusterSize){
+			printf("Fwrite failed. Exit.\n");
+            Fclose(fout);
+            return ERROR_GENERIC;
+		}	
+		BytesWritten += count;
+		printf(".");
+	}
+
+    TimeCount = ((struct mx_epit *)count_timer.base)->epitcnr;
+    epit_counter_disable(&count_timer); //polling mode
+
+    free(WriteBuffer);
+    Fclose(fout);
+    FlushCache();
+
+    printf("\n*****************File Reading End********************\n");
+    TimeCount = (0xFFFFFFFF - TimeCount) / 1000;    //ms
+    printf("Total data written %d, time %d ms\n", GetFileSize(fout), TimeCount);
+
+	return 0;
+}
 
 int fat_read_speed_test(void)
 {

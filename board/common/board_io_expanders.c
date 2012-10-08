ae7880a1a126daa2b993353868132dcf5b121bf2
Chris Reed <creed@freescale.com>
2012-10-08 15:28:06 -0500

    Fixed some io expander issues with sabre_ai board.
    - Taking io expanders out of reset.
    - Configuring I2C3 steering.
    - Thanks to Ross McLuckie for the patch.

--- board/common/board_io_expanders.c
+++ board/common/board_io_expanders.c
@@ -7,12 +7,14 @@
 
 #include "hardware.h"
 #include "board_io_expanders.h"
+#include "registers/regsi2c.h"
+#include "registers/regsiomuxc.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 // Globals
 ////////////////////////////////////////////////////////////////////////////////
 
-struct imx_i2c_request max7310_i2c_req_array[MAX7310_NBR];
+imx_i2c_request_t max7310_i2c_req_array[MAX7310_NBR];
 
 ////////////////////////////////////////////////////////////////////////////////
 // Code
@@ -20,17 +22,35 @@ struct imx_i2c_request max7310_i2c_req_array[MAX7310_NBR];
 
 void board_ioexpander_init(void)
 {
+#if defined(BOARD_SABRE_AI)
+    // Bring max7310 out of reset
+    // (this iomux setting should be done in gpio_iomux_config())
+    HW_IOMUXC_SW_MUX_CTL_PAD_SD2_DAT0_WR(BF_IOMUXC_SW_MUX_CTL_PAD_SD2_DAT0_MUX_MODE_V(ALT5));
+    gpio_set_direction(GPIO_PORT1, 15, GPIO_GDIR_OUTPUT);
+    gpio_set_level(GPIO_PORT1, 15, GPIO_HIGH_LEVEL);
+ 
+    // for I2C3 steering
+    // Select ALT5 mode of EIM_A24 for GPIO5_4 - EIMD18_I2C3_STEER(EIM_A24)
+    // high output to select I2C3 option
+    // (this iomux setting should be done in gpio_iomux_config())
+    HW_IOMUXC_SW_MUX_CTL_PAD_EIM_A24_WR(BF_IOMUXC_SW_MUX_CTL_PAD_EIM_A24_MUX_MODE_V(ALT5));
+    gpio_set_direction(GPIO_PORT5, 4, GPIO_GDIR_OUTPUT);
+    gpio_set_level(GPIO_PORT5, 4, GPIO_HIGH_LEVEL);
+#endif
+
+    // Build array of I2C request structures.
 #if defined(BOARD_EVB) || defined(BOARD_SABRE_AI)
     // Configure some board signals through I/O expanders
-    max7310_i2c_req_array[0].ctl_addr = I2C3_BASE_ADDR; // the I2C controller base address
+    max7310_i2c_req_array[0].ctl_addr = HW_I2C3; // the I2C controller base address
     max7310_i2c_req_array[0].dev_addr = MAX7310_I2C_ID0;    // the I2C DEVICE address
     max7310_init(0, MAX7310_ID0_DEF_DIR, MAX7310_ID0_DEF_VAL);
-    max7310_i2c_req_array[1].ctl_addr = I2C3_BASE_ADDR; // the I2C controller base address
+    
+    max7310_i2c_req_array[1].ctl_addr = HW_I2C3; // the I2C controller base address
     max7310_i2c_req_array[1].dev_addr = MAX7310_I2C_ID1;    // the I2C DEVICE address
     max7310_init(1, MAX7310_ID1_DEF_DIR, MAX7310_ID1_DEF_VAL);
 #endif
 #if defined(BOARD_SABRE_AI)
-    max7310_i2c_req_array[2].ctl_addr = I2C3_BASE_ADDR; // the I2C controller base address
+    max7310_i2c_req_array[2].ctl_addr = HW_I2C3; // the I2C controller base address
     max7310_i2c_req_array[2].dev_addr = MAX7310_I2C_ID2;    // the I2C DEVICE address
     max7310_init(2, MAX7310_ID2_DEF_DIR, MAX7310_ID2_DEF_VAL);
 #endif
--- board/common/board_io_expanders.h
+++ board/common/board_io_expanders.h
@@ -17,27 +17,26 @@
 #if defined(BOARD_SMART_DEVICE) || defined(BOARD_SABRE_LITE)
 #define MAX7310_NBR 0
 #endif
-/*******************************************************
- *      I/O expander MAX7310 I2C settings
- *******************************************************/
+
 /* For the ARD board which has 3 MAX7310 */
 #ifdef BOARD_SABRE_AI
 #define MAX7310_NBR 3
+
 /* I/O expander A */
-#define MAX7310_I2C_BASE_ID0  I2C3_BASE_ADDR
-#define MAX7310_I2C_ID0          (0x30 >> 1)
+#define MAX7310_I2C_BASE_ID0     3
+#define MAX7310_I2C_ID0          0x30
 #define MAX7310_ID0_DEF_DIR      0x00   // init direction for the I/O
 #define MAX7310_ID0_DEF_VAL      0xFF   // init value for the output
 
 /* I/O expander B */
-#define MAX7310_I2C_BASE_ID1  I2C3_BASE_ADDR
-#define MAX7310_I2C_ID1          (0x32 >> 1)
+#define MAX7310_I2C_BASE_ID1     3
+#define MAX7310_I2C_ID1          0x32
 #define MAX7310_ID1_DEF_DIR      0x00   // init direction for the I/O
 #define MAX7310_ID1_DEF_VAL      0xE7   // init value for the output
 
 /* I/O expander C */
-#define MAX7310_I2C_BASE_ID2  I2C3_BASE_ADDR
-#define MAX7310_I2C_ID2          (0x34 >> 1)
+#define MAX7310_I2C_BASE_ID2     3
+#define MAX7310_I2C_ID2          0x34
 #define MAX7310_ID2_DEF_DIR      0x00   // init direction for the I/O
 #define MAX7310_ID2_DEF_VAL      0x57   // init value for the output
 #endif
@@ -45,18 +44,20 @@
 #ifdef BOARD_EVB
 /* For the EVB board which has 2 MAX7310 */
 #define MAX7310_NBR 2
+
 /* Number 1 controls: BACKLIGHT_ON, PORT3_P114, CPU_PER_RST_B, PORT3_P110,
    PORT3_P105, PORT3_P112, PORT3_P107, PORT3_P109.
 */
-#define MAX7310_I2C_BASE_ID0  I2C3_BASE_ADDR
-#define MAX7310_I2C_ID0     (0x36 >> 1)
+#define MAX7310_I2C_BASE_ID0     3
+#define MAX7310_I2C_ID0          0x36
 #define MAX7310_ID0_DEF_DIR      0x00   // init direction for the I/O
 #define MAX7310_ID0_DEF_VAL      0xFF   // init value for the output
+
 /* Number 2 controls: CTRL_0, CTRL_1, CTRL_2, CTRL_3, CTRL_4, PORT3_P116,
    PORT2_P81, PORT3_P101
 */
-#define MAX7310_I2C_BASE_ID1  I2C3_BASE_ADDR
-#define MAX7310_I2C_ID1     (0x3E >> 1)
+#define MAX7310_I2C_BASE_ID1     3
+#define MAX7310_I2C_ID1          0x3E
 #define MAX7310_ID1_DEF_DIR      0x00   // init direction for the I/O
 #define MAX7310_ID1_DEF_VAL      0x09   // init value for the output
 #endif
@@ -65,7 +66,10 @@
 // Globals
 ////////////////////////////////////////////////////////////////////////////////
 
-//! @brief Create an array of I2C requests for all used expanders on the board.
+//! @brief Array of I2C requests for all used expanders on the board.
+//!
+//! This array is primarily used to pass details about I2C addresses and ports
+//! to the max7310 driver.
 extern imx_i2c_request_t max7310_i2c_req_array[];
 
 ////////////////////////////////////////////////////////////////////////////////

dbeb74501e08ff703c9907d8d620d7cd610e3b70
Chris Reed <creed@freescale.com>
2012-11-09 18:36:40 -0600

    Set SDK version to 1.0.

--- sdk/include/sdk_version.h
+++ sdk/include/sdk_version.h
@@ -43,7 +43,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 //! @brief Current version of the SDK.
-#define SDK_VERSION_STRING	"1.0b2"
+#define SDK_VERSION_STRING	"1.0"
 
 //! @name SDK version number components
 //@{

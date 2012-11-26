ccb11dd1dbcf9e25f915444bc575544630a71cfb
Chris Williamson <chris.williamson@freescale.com>
2012-11-26 10:44:18 -0600

    Remove test specific file from drivers/eim/src.
    Signed-off-by: Chris Williamson <chris.williamson@freescale.com>

--- apps/obds/src/tests/weim_nor_flash_test.c
+++ apps/obds/src/tests/weim_nor_flash_test.c
@@ -29,7 +29,7 @@
  */
 
 /*!
- * @defgroup diag_weim_nor Driver and test for a NOR flash connected on the WEIM.
+ * @defgroup diag_eim_nor Driver and test for a NOR flash connected on the EIM.
  */
 
 /*!
@@ -111,4 +111,85 @@ test_return_t eim_nor_flash_test(void)
     }
 }
 
-//RUN_TEST("WEIM NOR FLASH", weim_nor_flash_test)
+/*! General programmer's note: when writing to the flash, we are left shifting the address by 1 to align
+ * with 16-bit addressing.  For example, if we are writing to flash address of 0x555, the
+ * internal AHB address needs to be 0xAAA or "0x555<<1"
+ */
+
+// CS_BASE informs the program of the base system address of the Flash
+#define CS_BASE  0x08000000 // WEIM_CS_BASE_ADDR
+
+//reset & unlock writes
+#define reset() writew(0x00F0, CS_BASE)
+
+#define unlock() writew(0x00AA, CS_BASE+(0x555<<1)); writew(0x0055, CS_BASE+(0x2AA<<1))
+
+//manufacturer and device ID values
+#define MANU_ID 0x0020
+#define MANU_ID_AD readw(CS_BASE)
+#define DEV_ID_1 0x227E
+#define DEV_ID_AD_1 (CS_BASE + (0x1<<1))
+#define DEV_ID_2 0x2222
+#define DEV_ID_AD_2 (CS_BASE + (0xE<<1))
+#define DEV_ID_3 0x2201
+#define DEV_ID_AD_3 (CS_BASE + (0xF<<1))
+
+/*!
+ * This function reads and checks the manufacturer ID of the Numonyx M29W256GL NOR flash.
+ *
+ * @return 0 on success; non-zero otherwise
+ */
+int m29w256gl_auto_manu_id(void)
+{
+    short temp;
+    const char* indent = menu_get_indent();
+
+    reset();
+    unlock();
+    writew(0x0090, CS_BASE + (0x555 << 1));
+    temp = MANU_ID_AD;
+
+    printf("%sMANU_ID = 0x%04X\n", indent, temp);   // uncomment for debug purposes
+
+    reset();
+    if (temp == MANU_ID) {
+        printf("%s  WEIM NOR flash Manufacturer ID correct.\n", indent);
+        return 0;
+    } else {
+        printf("%s  **Manufacturer ID is not correct, it read back as 0x%04X\n", indent, temp);
+        printf("%s  **But it should be: 0x%04X\n", indent, MANU_ID);
+        return 1;
+    }
+}
+
+/*!
+ * This function reads and checks the device ID of the Numonyx M29W256GL NOR flash.
+ *
+ * @return 0 on success; non-zero otherwise
+ */
+int m29w256gl_auto_dev_id(void)
+{
+    short temp[3];
+    const char* indent = menu_get_indent();
+
+    reset();
+    unlock();
+    writew(0x0090, CS_BASE + (0x555 << 1));
+
+    temp[0] = readw(DEV_ID_AD_1);
+    temp[1] = readw(DEV_ID_AD_2);
+    temp[2] = readw(DEV_ID_AD_3);
+
+    reset();
+    printf("%sDEV_ID = 0x%04X, 0x%04X, 0x%04X\n", indent, temp[0], temp[1], temp[2]);   // uncomment for debug purposes
+
+    if ((temp[0] == DEV_ID_1) && (temp[1] == DEV_ID_2) && (temp[2] == DEV_ID_3)) {
+        printf("%s  WEIM NOR flash Device ID correct.\n", indent);
+        return 0;
+    } else {
+        printf("%s  **Device ID is not correct, it reads back as: 0x%04X, 0x%04X, 0x%04X\n", indent, temp[0],
+               temp[1], temp[2]);
+        printf("%s  **But it should be: 0x%04X, 0x%04X, 0x%04X\n", indent, DEV_ID_1, DEV_ID_2, DEV_ID_3);
+        return 1;
+    }
+}

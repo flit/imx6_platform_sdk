1096f3454dc49f0cb429727ef7dfbd1ea21f89a1
Sheng Nan <b38800@freescale.com>
2012-12-13 18:19:10 +0800

    i2c: hide the irq_id inside i2c driver
    - use instance to find irq_id when setup i2c interrupt.
    
    Signed-off-by: Sheng Nan <b38800@freescale.com>

--- sdk/drivers/i2c/src/imx_i2c.c
+++ sdk/drivers/i2c/src/imx_i2c.c
@@ -46,6 +46,10 @@
 //! Set this macro to 1 to enable tracing of data send and receive.
 #define TRACE_I2C 0
 
+//! @brief Get the irq id of I2C by instance number.
+//! @param x I2C instance number, from 1 through 3.
+#define I2C_IRQS(x) ( (x) == HW_I2C1 ? IMX_INT_I2C1 : (x) == HW_I2C2 ? IMX_INT_I2C2 : (x) == HW_I2C3 ? IMX_INT_I2C3 : 0xFFFFFFFF)
+
 ////////////////////////////////////////////////////////////////////////////////
 // Constants
 ////////////////////////////////////////////////////////////////////////////////
@@ -465,8 +469,10 @@ int i2c_write(const imx_i2c_request_t *rq)
     return i2c_xfer(rq, I2C_WRITE);
 }
 
-void i2c_setup_interrupt(uint32_t instance, uint32_t irq_id, void (*irq_subroutine)(void), bool state)
+void i2c_setup_interrupt(uint32_t instance, void (*irq_subroutine)(void), bool state)
 {
+    uint32_t irq_id = I2C_IRQS(instance);
+
     if (state) {
         // register the IRQ sub-routine 
         register_interrupt_routine(irq_id, irq_subroutine);
